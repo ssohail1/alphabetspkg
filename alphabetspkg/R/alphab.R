ea3998c01b8aaa50eb8cd8c6babcfec8c07adf0c
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-11-09 15:08:02 -0600

    Delete alphabetspkg directory

--- alphabetspkg/R/alphab.R
+++
@@ -1,6 +0,0 @@
-alphab <-
-function() {
-  alphabets <- c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z')
-  m <- sample(alphabets,1)
-  return(m)
-}
